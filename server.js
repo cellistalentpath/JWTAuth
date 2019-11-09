a212c2530a047f7fb362f8cde421f9dd35310eea
CJ Ellis <cellis@talentpath.com>
2019-11-09 12:12:28 -0600

    added some jwt

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const { GenerateJWT, DecodeJWT, ValidateJWT } = require("./dec-inc.js");
+
 const app = express();
-const port = 3000;
+//app.disable("x-powered-by");
+app.use(express.json());
+// app.header("Access-Control-Allow-Origin", "*");
+const port = process.env.PORT || 3100;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+let data = '{ "name" : "CJ" }';
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//const welcomeMessage = JSON.parse(message);
+
+app.get("/", (req, res) => res.send(data));
+
+app.post("/api/GenerateJWT", (req, res) =>
+	res.json(GenerateJWT(req.body.header, req.body.claims, req.body.key))
+);
+app.post("/api/DecodeJWT", (req, res) => res.json(DecodeJWT(req.body.sJWS)));
+app.post("/api/ValidateJWT", (req, res) =>
+	res.json(ValidateJWT(req.body.header, req.body.token, req.body.key))
+);
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
